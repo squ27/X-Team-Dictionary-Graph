92cbcb413c20d481cc12e5ff182e77cb8c54bde9
Suyan Qu <squ27@wisc.edu>
2018-04-09 20:59:41 -0500

    Complete merging. Graph class now works.

--- src/Graph.java
+++ src/Graph.java
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 //Assignment Name: P4 Dictionary Graph
 //File Name: Graph.java
-<<<<<<< HEAD
 //Author: Neal Pongmorrakot
-=======
 //Author: X-Team 67/Neal Pongmorrakot
->>>>>>> NealPongmorrakot
 //Email: pongmorrakot@wisc.edu
 //Due Date: Apr 16, 2018
 //Other Source: -
@@ -160,80 +156,3 @@ public class Graph<E> implements GraphADT<E> {
     }
 
 }
-=======
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
-/**
- * Undirected and unweighted graph implementation
- * 
- * @param <E> type of a vertex
- * 
- * @author sapan (sapan@cs.wisc.edu)
- * 
- */
-public class Graph<E> implements GraphADT<E> {
-    
-    /**
-     * Instance variables and constructors
-     */
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public E addVertex(E vertex) {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public E removeVertex(E vertex) {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean addEdge(E vertex1, E vertex2) {
-        
-    }    
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean removeEdge(E vertex1, E vertex2) {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean isAdjacent(E vertex1, E vertex2) {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Iterable<E> getNeighbors(E vertex) {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Iterable<E> getAllVertices() {
-        
-    }
-
-}
->>>>>>> 53682ae67e2f754f03afd53ec916aaf26ede1250
